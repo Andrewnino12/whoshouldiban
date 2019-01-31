4ea627549392f09e1eb82f14b8fac6e078c523e4
Andrew Marshall <andydangermarshall@gmail.com>
2019-01-31 01:15:24 -0500

    Fixes bug with appending api_key to request.

--- functions.php
+++ functions.php
@@ -408,7 +408,7 @@ function get_headers_from_curl_response($response)
 function makeRequest($requestUrl)
 {
     global $api_key;
-    $encodedUrl = strpos($requestUrl, 'queue=') > 0 ?  $requestUrl . "&api_key=$api_key" : $requestUrl . "?api_key=$api_key";
+    $encodedUrl = strpos($requestUrl, 'queue=') ?  $requestUrl . "&api_key=$api_key" : $requestUrl . "?api_key=$api_key";
 
     $ch = curl_init();
 
